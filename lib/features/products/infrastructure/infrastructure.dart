fb187d733a103a434e214a92952bd6350581796f
anyel-ec <paulangel826@gmail.com>
2024-05-30 18:53:09 -0500

    add product provider the state

---
+++ lib/features/products/infrastructure/infrastructure.dart
@@ -0,0 +1,5 @@
+
+export 'package:teslo_shop/features/products/domain/datasources/products_datasource.dart';
+export 'package:teslo_shop/features/products/domain/repositories/products_repository.dart';
+export 'package:teslo_shop/features/products/infrastructure/mappers/product_mapper.dart';
+export 'package:teslo_shop/features/products/infrastructure/repositories/products_repository_impl.dart';
